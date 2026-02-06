5ba2521743beb82297fd3b970e1a506adf108816
Dylan Pruitt <dpruitt0703@gmail.com>
2026-02-05 21:40:27 -0500

    feat(ui): added basic grid display

--- main.go
+++ main.go
@@ -5,15 +5,39 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	lipgloss "github.com/charmbracelet/lipgloss"
 )
 
+var normalStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#dfdfdf")).Background(lipgloss.Color("#000000"))
+var cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#000000")).Background(lipgloss.Color("#dfdfdf"))
+var cursorLineStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#000000")).Background(lipgloss.Color("#9f9f9f"))
+
+type TileType int
+
+const (
+	TilePlains TileType = iota
+	TileMountain
+)
+
+const TileChars string = ".^"
+
+type Tile struct {
+	tileType TileType
+}
+
 type model struct {
-	hello string
+	hello   string
+	tileMap [15][30]Tile
+	cursorX int
+	cursorY int
 }
 
 func initialModel() model {
 	return model{
-		hello: "Hello World",
+		hello:   "Hello World",
+		tileMap: [15][30]Tile{},
+		cursorX: 5,
+		cursorY: 7,
 	}
 }
 
@@ -25,6 +49,24 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
+		case "up":
+			if m.cursorY > 0 {
+				m.cursorY -= 1
+			}
+		case "down":
+			if m.cursorY < 14 {
+				m.cursorY += 1
+			}
+		case "left":
+			if m.cursorX > 0 {
+				m.cursorX -= 1
+			}
+		case "right":
+			if m.cursorX < 29 {
+				m.cursorX += 1
+			}
+		case "enter":
+			m.tileMap[m.cursorY][m.cursorX].tileType = TileMountain
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
@@ -36,7 +78,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	s := m.hello
+	s := m.hello + "\n"
+	for i := 0; i < 15; i++ {
+		for j := 0; j < 30; j++ {
+			textStyle := normalStyle
+			if m.cursorX == j && m.cursorY == i {
+				textStyle = cursorStyle
+			} else if m.cursorX == j || m.cursorY == i {
+				textStyle = cursorLineStyle
+			}
+			s += textStyle.Render(string(TileChars[m.tileMap[i][j].tileType]))
+		}
+		s += "\n"
+	}
 	s += "\nPress q to quit.\n"
 
 	return s
