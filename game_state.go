03c3697ac6a71a65c4107aabebb37959e3e54b65
Dylan Pruitt <dpruitt0703@gmail.com>
2026-02-12 21:11:51 -0500

    feat(refactor): refactored game state code out to separate file

---
+++ game_state.go
@@ -0,0 +1,339 @@
+package main
+
+import (
+	"math"
+	"slices"
+
+    lipgloss "github.com/charmbracelet/lipgloss"
+)
+
+const mapSizeX int = 30
+const mapSizeY int = 15
+
+type TileType int
+
+const (
+	TilePlains TileType = iota
+	TileMountain
+)
+
+const TileChars string = ".^"
+
+type Feature int
+
+const (
+	FeatureNone Feature = iota
+	FeatureVillage
+	FeatureCity
+	FeatureCrop
+	FeatureFarm
+)
+
+const FeatureChars string = " +@,#"
+
+type Civ struct {
+	name      string
+	id        int
+	tileStyle lipgloss.Style
+}
+
+type City struct {
+	name       string
+	population int
+	owner      *Civ
+	positionX  int
+	positionY  int
+}
+
+type Tile struct {
+	tileType       TileType
+	feature        Feature
+	city           *City
+	validForAction bool
+	discoveredBy   []int
+}
+
+func (t Tile) discoveredByPlayer() bool {
+	// assumes player ID will always be 0
+	return slices.Contains(t.discoveredBy, 0)
+}
+
+type UnitType int
+
+const (
+	UnitWarrior UnitType = iota
+)
+
+const UnitChars string = "w"
+
+type Unit struct {
+	name       string
+	unitType   UnitType
+	hp         int
+	maxHp      int
+	attack     int
+	defense    int
+	kills      int
+	positionX  int
+	positionY  int
+	owner      *Civ
+	movePoints int
+}
+
+func (u *Unit) moveTo(x, y int) {
+	u.positionX = x
+	u.positionY = y
+}
+
+func (u *Unit) Attack(o *Unit) {
+	attackerDamage := int(math.Round(float64(u.attack) * (float64(u.hp) / float64(u.maxHp))))
+	defenderDamage := int(math.Round(float64(o.defense) * (float64(o.hp) / float64(o.maxHp))))
+
+	o.hp -= attackerDamage
+	if o.hp > 0 {
+		u.hp -= defenderDamage
+		if u.hp <= 0 {
+			o.kills++
+		}
+	} else {
+		u.moveTo(o.positionX, o.positionY)
+		u.kills++
+	}
+}
+
+func makeWarrior(x, y int, owner *Civ) Unit {
+	return Unit{
+		name:       "Warrior",
+		unitType:   UnitWarrior,
+		hp:         4,
+		maxHp:      4,
+		attack:     3,
+		defense:    3,
+		positionX:  x,
+		positionY:  y,
+		owner:      owner,
+		movePoints: 2,
+	}
+}
+
+type GameState struct {
+    tileMap      [mapSizeY][mapSizeX]Tile
+	civs         []Civ
+	units        []Unit
+}
+
+func initialGameState() GameState {
+	civ0 := Civ{
+		name:      "TestCiv",
+		id:        0,
+		tileStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("#dfdfdf")).Background(lipgloss.Color("#6f0000")),
+	}
+	civ1 := Civ{
+		name:      "TestCiv2",
+		id:        1,
+		tileStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("#dfdfdf")).Background(lipgloss.Color("#001f5f")),
+	}
+	g := GameState{
+		tileMap: [mapSizeY][mapSizeX]Tile{},
+		civs: []Civ{
+			civ0,
+			civ1,
+		},
+		units: []Unit{
+			makeWarrior(6, 6, &civ0),
+			makeWarrior(7, 7, &civ0),
+			makeWarrior(8, 6, &civ1),
+		},
+	}
+	g.tileMap[5][9].tileType = TileMountain
+	g.tileMap[7][7].tileType = TileMountain
+	g.tileMap[7][10].tileType = TileMountain
+	g.tileMap[4][2].tileType = TileMountain
+	g.tileMap[4][3].tileType = TileMountain
+	g.tileMap[4][4].tileType = TileMountain
+	g.tileMap[6][7].feature = FeatureVillage
+	g.tileMap[8][8].feature = FeatureVillage
+	g.tileMap[8][9].feature = FeatureCrop
+	g.tileMap[8][10].feature = FeatureCrop
+	g.tileMap[8][11].feature = FeatureCrop
+	g.tileMap[8][12].feature = FeatureCrop
+	g.tileMap[11][22].feature = FeatureVillage
+	g.revealTilesFromPos(6, 6, 1, &civ0)
+
+	return g
+}
+
+
+
+func (g *GameState) createFarm(x, y int) {
+	if g.tileMap[y][x].city == nil {
+		return
+	}
+	g.tileMap[y][x].feature = FeatureFarm
+	g.cultureBombTile(*g.tileMap[y][x].city, x, y)
+	g.revealTilesFromPos(x, y, 1, g.tileMap[y][x].city.owner)
+}
+
+func (g *GameState) captureVillageAtPositionWithUnit(x, y int, u *Unit) {
+	if g.tileMap[y][x].feature != FeatureVillage {
+		return
+	}
+	g.createCity(u.owner, x, y)
+}
+
+func (g *GameState) createCity(civ *Civ, x, y int) {
+	name := "Rome"
+	if civ.name == "TestCiv2" {
+		name = "London"
+	}
+	city := City{
+		name:       name,
+		population: 1,
+		owner:      civ,
+		positionX:  x,
+		positionY:  y,
+	}
+
+	g.tileMap[y][x].feature = FeatureCity
+	g.cultureBombTile(city, x, y)
+	g.revealTilesFromPos(x, y, 2, civ)
+}
+
+func (g *GameState) runCombat(attacker *Unit, defender *Unit) {
+	attacker.Attack(defender)
+
+	delIndex := -1
+	for i := 0; i < len(g.units); i++ {
+		if &g.units[i] == attacker && attacker.hp <= 0 {
+			delIndex = i
+		}
+	}
+	if delIndex > -1 {
+		slices.Delete(g.units, delIndex, delIndex+1)
+	}
+	for i := 0; i < len(g.units); i++ {
+		if &g.units[i] == defender && defender.hp <= 0 {
+			delIndex = i
+		}
+	}
+	if delIndex > -1 {
+		slices.Delete(g.units, delIndex, delIndex+1)
+	}
+}
+
+func (g *GameState) cultureBombTile(city City, x, y int) {
+	for i := -1; i < 2; i++ {
+		for j := -1; j < 2; j++ {
+			if x+j >= 0 && x+j < mapSizeX && y+i >= 0 && y+i < mapSizeY && g.tileMap[y+i][x+j].city == nil && tileInCityRange(x+j, y+i, city) {
+				g.tileMap[y+i][x+j].city = &city
+			}
+		}
+	}
+}
+
+func (g *GameState) captureCityAtPositionWithUnit(x, y int, u *Unit) {
+	if g.tileMap[y][x].feature != FeatureCity || g.tileMap[y][x].city.owner == u.owner {
+		return
+	}
+	g.tileMap[y][x].city.owner = u.owner
+}
+
+func tileInCityRange(x, y int, city City) bool {
+	xDist := city.positionX - x
+	if xDist < 0 {
+		xDist *= -1
+	}
+	yDist := city.positionY - y
+	if yDist < 0 {
+		yDist *= -1
+	}
+	return xDist <= 3 && yDist <= 3
+}
+
+type TileCost struct {
+	totalCost int
+	baseCost  int
+}
+
+func (g *GameState) setValidMoveTilesForUnit(u *Unit) {
+	tileCosts := [mapSizeY][mapSizeX]TileCost{}
+	validTiles := [][2]int{}
+	// reset tile costs, set tile unit is on to valid
+	for i := 0; i < mapSizeY; i++ {
+		for j := 0; j < mapSizeX; j++ {
+			if i == u.positionY && j == u.positionX {
+				tileCosts[i][j].totalCost = 0
+				tileCosts[i][j].baseCost = 0
+				validTiles = append(validTiles, [2]int{u.positionX, u.positionY})
+			} else {
+				tileCosts[i][j].totalCost = 99
+				tileCosts[i][j].baseCost = g.getTileMoveCost(j, i, u)
+				g.tileMap[i][j].validForAction = false
+			}
+		}
+	}
+
+	t := 0
+	for t < len(validTiles) {
+		tilePos := validTiles[t]
+		thisTc := tileCosts[tilePos[1]][tilePos[0]]
+		for i := -1; i < 2; i++ {
+			for j := -1; j < 2; j++ {
+				if tilePos[0]+j >= 0 && tilePos[0]+j < mapSizeX && tilePos[1]+i >= 0 && tilePos[1]+i < mapSizeY {
+					tc := &tileCosts[tilePos[1]+i][tilePos[0]+j]
+					oldTotalCost := tc.totalCost
+					newTotalCost := thisTc.totalCost + tc.baseCost
+					if newTotalCost < oldTotalCost {
+						tc.totalCost = newTotalCost
+					}
+
+					tilePos := [2]int{tilePos[0] + j, tilePos[1] + i}
+					if tc.totalCost <= u.movePoints && !slices.Contains(validTiles, tilePos) {
+						validTiles = append(validTiles, tilePos)
+					}
+				}
+			}
+		}
+		t++
+	}
+
+	for i := 0; i < len(validTiles); i++ {
+		t := validTiles[i]
+		g.tileMap[t[1]][t[0]].validForAction = true
+	}
+}
+
+func (g *GameState) getTileMoveCost(x, y int, u *Unit) int {
+	const IMPASSABLE int = 99
+	unitOnTile := g.getUnitOnTile(x, y)
+	if unitOnTile != nil && unitOnTile.owner == u.owner && (u.positionX != unitOnTile.positionX || u.positionY != unitOnTile.positionY) {
+		return IMPASSABLE
+	} else {
+		switch g.tileMap[y][x].tileType {
+		case TileMountain:
+			// TODO handle case where Mountains are impassable without Climbing tech
+			return 2
+		default:
+			return 1
+		}
+	}
+}
+
+func (g *GameState) revealTilesFromPos(x, y, revealRange int, c *Civ) {
+	for i := -revealRange; i < revealRange+1; i++ {
+		for j := -revealRange; j < revealRange+1; j++ {
+			if x+j >= 0 && x+j < mapSizeX && y+i >= 0 && y+i < mapSizeY {
+				g.tileMap[y+i][x+j].discoveredBy = append(g.tileMap[y+i][x+j].discoveredBy, c.id)
+			}
+		}
+	}
+}
+
+func (g GameState) getUnitOnTile(x, y int) *Unit {
+	for i := 0; i < len(g.units); i++ {
+		if g.units[i].positionX == x && g.units[i].positionY == y {
+			return &g.units[i]
+		}
+	}
+	return nil
+}
